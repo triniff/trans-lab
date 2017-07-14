daa4024d83106c06fcea3b99d060148a9b31a411
“Trinidad <tfuentesfuentealba@gmail.com>
2017-07-14 13:14:55 -0300

    arreglando menu y links

--- js/main.js
+++ js/main.js
@@ -29,32 +29,43 @@ $(document).ready(function() {
 	
 
 	/*Funcion hamburgesa*/
-	var trigger = $('.hamburger'),
-    overlay = $('.overlay'),
-    isClosed = false;
+	(function hamburgesa(){
+		var trigger = $('.hamburger'),
+	    overlay = $('.overlay'),
+	    isClosed = false;
+	
+	    trigger.click(function () {
+	      hamburger_cross();      
+	    });
+	
+	    function hamburger_cross() {
+	
+	      if (isClosed == true) {          
+	        overlay.hide();
+	        trigger.removeClass('is-open');
+	        trigger.addClass('is-closed');
+	        isClosed = false;
+	      } else {   
+	        overlay.show();
+	        trigger.removeClass('is-closed');
+	        trigger.addClass('is-open');
+	        isClosed = true;
+	      }
+	     }
+	     overlay.click(function () {
+	     	overlay.hide();
+	        trigger.removeClass('is-open');
+	        trigger.addClass('is-closed');
+	        isClosed = false;
+        	$('#wrapper').toggleClass('toggled');
+	    })
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
+	  })();
   
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   }); 
+
 
   /*funcion para agregar tarjetas*/
   	function recuperarDatosTarjeta(){
